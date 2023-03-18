57ea47e95b2e275cf770adf1c621a5f2fc64d4f6
lyae <lyaesley@gmail.com>
2023-03-18 22:52:47 +0900

    [Programmers] Level1 java 삼총사

---
+++ src/programmers/test/trio.java
@@ -0,0 +1,26 @@
+package programmers.test;
+
+
+class trio {
+    public static int solution(int[] number) {
+        int answer = 0;
+
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] number = {-2,3,0,2,-5}; //result : 2
+        System.out.println(solution(number));
+    }
+}
+
