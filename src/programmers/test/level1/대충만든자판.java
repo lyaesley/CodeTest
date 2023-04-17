a9c2fdf5c40dd63b5442d98640ec5551c0874487
lyae <lyaesley@gmail.com>
2023-04-17 21:43:29 +0900

    [Programmers] test.level1

--- src/programmers/test/level1/대충만든자판.java
+++ src/programmers/test/level1/대충만든자판.java
@@ -32,8 +32,8 @@ public class 대충만든자판 {
     }
 
     public static void main(String[] args) {
-        String[] keymap = {"AAAAAAAAAAAAAA12"};
-        String[] targets = {"BBBBBBBBBBBBBB12"};
+        String[] keymap = {"AAAAAAAAAAAAAA1213"};
+        String[] targets = {"BBBBBBBBBBBBBB1213"};
         solution(keymap, targets);
     }
 }
